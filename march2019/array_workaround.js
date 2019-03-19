4337528c3083b8eb1df6df8c649c5f10d06cc6b0
dedosfrios93 <jotagalvan@gmail.com>
2019-03-19 17:17:30 -0500

    This repo will contain the latest workarounds in JS

---
+++ march2019/array_workaround.js
@@ -0,0 +1,21 @@
+var girls = ["pamela","susan","wendy","alessandra","jenny"];
+
+
+//cambiar el orden de un Array
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
+
+
+//funcion para cambiar el orden del mismo array (sin producir un nuevo array)
+function prueba (cuenta){
+	let count = cuenta
+	for (let i = 0; i < Math.floor(count/2); i++){
+		console.log(i)
+	}
+}
