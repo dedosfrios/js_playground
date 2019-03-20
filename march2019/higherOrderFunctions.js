c38533dbd07df0e9e6657591d86c6c2aac998f6b
dedosfrios93 <jotagalvan@gmail.com>
2019-03-20 14:18:34 -0500

    Creando un Array ajustando el Index html

--- march2019/higherOrderFunctions.js
+++ march2019/higherOrderFunctions.js
@@ -1,1 +1,8 @@
-higherOrderFunctions.js
+//Creating a new array with the Object array and the array methods.
+let simpleArray = new Array(10).fill().map(ele => ele = Math.floor(Math.random()*10));
+console.log(simpleArray);
+
+
+
+
+
