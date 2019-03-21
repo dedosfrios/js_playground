4a5a3cbf22264b173783d64bade24c186fc99beb
dedosfrios93 <jotagalvan@gmail.com>
2019-03-20 19:15:24 -0500

    actualizacion rapida

--- march2019/higherOrderFunctions.js
+++ march2019/higherOrderFunctions.js
@@ -1,8 +1,32 @@
 //Creating a new array with the Object array and the array methods.
 let simpleArray = new Array(10).fill().map(ele => ele = Math.floor(Math.random()*10));
-console.log(simpleArray);
+//console.log(simpleArray);
+
+/*
+//log 10 times the word hello
+for (let i = 0; i < 10 ; ++i){
+	console.log("hello")
+}
+
+//Functio that take a function value as part of 
+//its parameters
+
+function repeat (n, action){
+	for(let i = 0; i < n; i++){
+		action(i)
+	}
+}
+
+repeat(8,console.log);
+*/
 
 
-
-
+function noisy (f){
+	return function(...args){
+		console.log("calling with ", args);
+		let result = f(...args); //closure
+		console.log("called with", args, ", returned", result)
+		return result;
+	}
+}
 
