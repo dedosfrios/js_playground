639319975a675d75e0e6040757a20404a2f634fe
dedosfrios93 <jotagalvan@gmail.com>
2019-03-31 13:45:33 -0500

    understanding of the binding in object literals

--- march2019/methodsOOP.js
+++ march2019/methodsOOP.js
@@ -4,3 +4,11 @@ rabbit.speak = function (line){
 	console.log("the rabbit say" + line);
 }
 
+
+// I didnt know you could assing methods creating objects literals
+// I thought that you need it to associate it with the .  
+function meow (){
+	console.log("meow, madafaka im the cat");
+}
+
+let cat ={type:"persian",meow}
