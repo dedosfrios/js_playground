2d2cea16ace666d7c9326cdb76db51f1a06b2876
dedosfrios93 <jotagalvan@gmail.com>
2019-03-31 19:31:51 -0500

    function constructor added

--- march2019/methodsOOP.js
+++ march2019/methodsOOP.js
@@ -9,6 +9,22 @@ rabbit.speak = function (line){
 // I thought that you need it to associate it with the .  
 function meow (){
 	console.log("meow, madafaka im the cat");
+	console.log(this);
 }
 
 let cat ={type:"persian",meow}
+
+//We cant use a declare function and use the call method on that function
+//to determinate from which object the this variables is gonna take is value
+
+
+let Cars = function (name,speedlimit,engine,sound){
+	this.name = name;
+	this.speedlimit = speedlimit;
+	this.engine = engine;
+	this.skrt = function(sound){
+		console.log(sound);
+	}
+}
+
+let audi2000 = new Cars("audi2000",350,"V8","brrrrrmmmmm");
